165ad169807fd925a2e7313d35442ba0b6856e8e
Arthur.chaix <arthur.chaix@isep.fr>
2017-05-18 16:13:29 +0200

    debut d'automatisations

--- Homecheckmvc/vue/automatisations.php
+++ Homecheckmvc/vue/automatisations.php
@@ -26,51 +26,30 @@ require "commun.php";
                 <div class="box3">
                             
 
-                        <table class="table2" >
+                        <form method="post" action="index.php??cible=action">
+                        <p>
+                        <input type="radio" name="action" value="chauffage" id="chauffage" class="td3"> <label for="chauffage"> Chauffage </label><br/>
+                         <input type="radio" name="action" value="ouverturevolets" id="ouverturevolets" class="td3"> <label for="ouverturevolets"> Ouverture des volets </label><br/>
+                          <input type="radio" name="action" value="fermeturevolets" id="fermeturevolets" class="td3"> <label for="fermeturevolets"> Fermeture des Volets </label><br/>
+                           <input type="radio" name="action" value="lumiere" id="lumiere" class="td3"> <label for="lumiere"> Lumiere </label><br/>
+                        </p>
+                        </form>
 
 
-                                <tr >
-                                     <td class="td3"> Chauffage </td>
-                                </tr>
-                                <tr>
-                                    <td class="td3" > Ouverture des volets </td>
-                                </tr>
-                                <tr>
-                                    <td class="td3"> Fermeture des volets </td>
-                                </tr>
-                                <tr>
-                                    <td class="td3" > Lumière </td>
-                                </tr>
-                    
-                        </table>
-                      
-                        <table class="table2" >
+                        <form class="table2" method="post" action="index.php?cible=piece" >
 
-                            <tr>
-                                <td class="td3"> Salon </td>
-                            </tr>
-                            <tr>
-                                <td class="td3" > Chambre 1  </td>
-                            </tr>
-                            <tr>
-                                <td class="td3"> Chambre 2 </td>
-                            </tr>
-                            <tr>
-                                <td class="td3"> Cuisine </td>
-                            </tr>
-                            <tr>
-                                <td class="td3"> Salle de bain 1 </td>
-                            </tr>
-                            <tr>
-                                <td class="td3"> Salle de bain 2 </td>
-                            </tr>
-                            <tr>
-                                <td class="td3"> Garage </td>
-                            </tr>
-                            <tr>
-                                <td class="td3"> Terrasse </td>
-                            </tr>
-                        </table>
+                            <p> <input type="checkbox" name="salon" id="salon" class="td3"/> <label for="salon"> Salon </label><br/>
+                                <input type="checkbox" name="chambre1" id="chambre1" class="td3"/> <label for="chambre1"> Chambre 1 </label><br/> 
+                                <input type="checkbox" name="chambre2" id="chambre2" class="td3"/> <label for="chambre2"> Chambre 2 </label><br/>
+                                <input type="checkbox" name="cuisine" id="cuisine" class="td3"/> <label for="cuisine"> Cuisine </label><br/>
+                                <input type="checkbox" name="sdb1" id="sdb1" class="td3"/> <label for="sdb1"> Salle de bain 1 </label><br/>
+                                <input type="checkbox" name="sdb2" id="sdb2" class="td3"/> <label for="sdb2"> Salle de bain 2 </label><br/>
+                                <input type="checkbox" name="garage" id="garage" class="td3"/> <label for="garage"> Garage </label><br/>
+                                <input type="checkbox" name="terrasse" id="terrasse" class="td3"/> <label for="terrasse"> Terrasse </label><br/>
+                            </p>
+                        </form>   
+
+                            
 
 
                             
@@ -92,35 +71,24 @@ require "commun.php";
                         </nav1>
                             
 
-                        <table class="table2" >
-                            
-                            <tr>
-                                <td class="td3"> Lundi </td>    
-                            </tr>
-                            <tr>    
-                                <td class="td3"> Mardi  </td>
-                            </tr>
-                            <tr>
-                                <td class="td3"> Mercredi </td>
-                            </tr>
-                            <tr>
-                                <td class="td3"> Jeudi </td>
-                            </tr>
-                            <tr>
-                                <td class="td3"> Vendredi </td>
-                            </tr>
-                            <tr>
-                                <td class="td3"> Samedi </td>
-                            </tr>
-                            <tr>
-                                <td class="td3"> Dimanche </td>
-                            </tr>
-                        </table>
+                        <form class="table2" method="post" action="index.php?cible=recurrence" >
+
+                            <p> <input type="checkbox" name="lundi" id="lundi" class="td3"/> <label for="lundi"> Lundi </label><br>
+                                <input type="checkbox" name="mardi" id="mardi" class="td3"/> <label for="mardi"> Mardi </label><br/>
+                                <input type="checkbox" name="Mercredi" id="Mercredi" class="td3"/> <label for="Mercredi"> Mercredi </label><br/>
+                                <input type="checkbox" name="Jeudi" id="Jeudi" class="td3"/> <label for="Jeudi"> Jeudi </label><br/>
+                                <input type="checkbox" name="Vendredi" id="Vendredi" class="td3"/> <label for="Vendredi"> Vendredi </label><br/>
+                                <input type="checkbox" name="Samedi" id="Samedi" class="td3"/> <label for="Samedi"> Samedi </label><br/>
+                                <input type="checkbox" name="dimanche" id="dimanche" class="td3"/> <label for="dimanche"> Dimanche </label><br/>
+                            </p>
+                            </form>                            
                 </div>
                 <div class="box5">
                     <a href="#"><div class="enre"> Enregistrer </div></a>
                 </div>
                 </div>
+
+                
                 <div class="box4">
                     <div class="A1"> A1 </div>
                     <div><a href="#"> <img class="croix1" src="image/croix.png" alt="supprimer automatisation" /></a></div>
@@ -145,4 +113,3 @@ require "commun.php";
 
     include 'gabarit.php';
 ?>
-
