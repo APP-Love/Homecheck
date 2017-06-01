321733134c1e2971f0ab291d1e287dde9db5625b
Arthur.chaix <arthur.chaix@isep.fr>
2017-06-01 09:59:01 +0200

    MAJ automatisations

--- Homecheckmvc2/vue/automatisations.php
+++ Homecheckmvc2/vue/automatisations.php
@@ -10,83 +10,85 @@ require "commun.php";
          
         <Section>
             <div id="box"> 
-                    <div class="automatisationtitre"> Les automatisations </div>
+                    <div class="automatisationtitre"> Les automatisations 
+                    </div>
                 <div class="box1">
                     
-                        <div class="Ajout" > Ajout d'automatisation : </div> 
-                <div class="box2">
+                     <div class="Ajout" > Ajout d'automatisation : 
+                    </div> 
+                
+
+                    <div class="box2">
                     
                         <div class="intitulé"> Action </div> 
                         <div class="intitulé" > Pièces </div>
                         <div class="intitulé"> Conditions </div>
                         <div class="intitulé" > Récurrence </div> 
                     
-                </div>
+                    </div>
+                    <form method="post" action="index.php?cible=auto">  
+                            <div class="box3">
+                    
+                                    <div>
+                                                <p>
+                                                <input type="radio" name="action" value="chauffage" id="chauffage" class="td3"> <label for="chauffage"> Chauffage </label><br/>
+                                                 <input type="radio" name="action" value="ouverturevolets" id="ouverturevolets" class="td3"> <label for="ouverturevolets"> Ouverture des volets </label><br/>
+                                                  <input type="radio" name="action" value="fermeturevolets" id="fermeturevolets" class="td3"> <label for="fermeturevolets"> Fermeture des Volets </label><br/>
+                                                   <input type="radio" name="action" value="lumiere" id="lumiere" class="td3"> <label for="lumiere"> Lumiere </label><br/>
+                                                </p>
+                                    </div>
 
-                <div class="box3">
-                            
 
-                        <form method="post" action="index.php??cible=action">
-                        <p>
-                        <input type="radio" name="action" value="chauffage" id="chauffage" class="td3"> <label for="chauffage"> Chauffage </label><br/>
-                         <input type="radio" name="action" value="ouverturevolets" id="ouverturevolets" class="td3"> <label for="ouverturevolets"> Ouverture des volets </label><br/>
-                          <input type="radio" name="action" value="fermeturevolets" id="fermeturevolets" class="td3"> <label for="fermeturevolets"> Fermeture des Volets </label><br/>
-                           <input type="radio" name="action" value="lumiere" id="lumiere" class="td3"> <label for="lumiere"> Lumiere </label><br/>
-                        </p>
-                        </form>
+                                    <div class="table2" >
 
-
-                        <form class="table2" method="post" action="index.php?cible=piece" >
-
-                            <p> <input type="checkbox" name="salon" id="salon" class="td3"/> <label for="salon"> Salon </label><br/>
-                                <input type="checkbox" name="chambre1" id="chambre1" class="td3"/> <label for="chambre1"> Chambre 1 </label><br/> 
-                                <input type="checkbox" name="chambre2" id="chambre2" class="td3"/> <label for="chambre2"> Chambre 2 </label><br/>
-                                <input type="checkbox" name="cuisine" id="cuisine" class="td3"/> <label for="cuisine"> Cuisine </label><br/>
-                                <input type="checkbox" name="sdb1" id="sdb1" class="td3"/> <label for="sdb1"> Salle de bain 1 </label><br/>
-                                <input type="checkbox" name="sdb2" id="sdb2" class="td3"/> <label for="sdb2"> Salle de bain 2 </label><br/>
-                                <input type="checkbox" name="garage" id="garage" class="td3"/> <label for="garage"> Garage </label><br/>
-                                <input type="checkbox" name="terrasse" id="terrasse" class="td3"/> <label for="terrasse"> Terrasse </label><br/>
-                            </p>
-                        </form>   
+                                            <p> <input type="radio" name="room" value="allroom" id="allroom" class="td3"/> <label for="allroom"> Toutes les pieces </label><br/>
+                                                  <input type="radio" name="room" value="chambres" id="chambres" class="td3"/> <label for="chambres"> Toutes les chambres </label><br/>
+                                                  <input type="radio" name="room" value="garageterrasse" id="garageterrasse" class="td3"/> <label for="garageterrasse"> Garage et terrasse </label><br/>                              
+                                                <input type="radio" name="room" id="cuisinesalon" value="cuisinesalon" class="td3"/> <label for="cuisinesalon"> Cuisine et salon </label><br/>
+                                                <input type="radio" name="room" value="sdb"  id="sdb" class="td3"/> <label for="sdb"> Salle(s) de bain </label><br/>
+                                              
+                                            </p>
+                                    </div>   
 
                             
-
-
-                            
-                        <nav1>
-                            <ul >
-                                <form class="données" method="post" action="retour.php" >
-                                        <label for=" Température" ></label> 
-                                        <input type="text" name="Température" id="Température" placeholder="Température..."/>
-                                </form>
-                                <form class="données" method="post" action="retour.php" >
-                                        <label for=" Heure de début" ></label> 
-                                        <input type="text" name="Heure de début" id="Heure de début" placeholder="Heure de début..."/>
-                                </form>
-                                <form class="données" method="post" action="retour.php" >
-                                        <label for=" Heure de fin" ></label> 
-                                        <input type="text" name="Heure de fin" id="Heure de fin" placeholder="Heure de fin..."/>
-                                </form>
-                            </ul>
-                        </nav1>
+                                    <nav1>
+                                                <ul >
+                                                    <div class="données" >
+                                                            <label for=" Température" ></label> 
+                                                            <input type="text" name="temperature" id="temperature" placeholder="Température..."/>
+                                                    </div>
+                                                    <div class="données" >
+                                                            <label for=" Heure de début" ></label> 
+                                                            <input type="text" name="Hdebut" id="Hdebut" placeholder="Heure de début..."/>
+                                                    </div>
+                                                    <div class="données"  >
+                                                            <label for=" Heure de fin" ></label> 
+                                                            <input type="text" name="Hfin" id="Hfin" placeholder="Heure de fin..."/>
+                                                    </div>
+                                                </ul>
+                                    </nav1>
                             
 
-                        <form class="table2" method="post" action="index.php?cible=recurrence" >
+                                     <div class="table2" >
 
-                            <p> <input type="checkbox" name="lundi" id="lundi" class="td3"/> <label for="lundi"> Lundi </label><br>
-                                <input type="checkbox" name="mardi" id="mardi" class="td3"/> <label for="mardi"> Mardi </label><br/>
-                                <input type="checkbox" name="Mercredi" id="Mercredi" class="td3"/> <label for="Mercredi"> Mercredi </label><br/>
-                                <input type="checkbox" name="Jeudi" id="Jeudi" class="td3"/> <label for="Jeudi"> Jeudi </label><br/>
-                                <input type="checkbox" name="Vendredi" id="Vendredi" class="td3"/> <label for="Vendredi"> Vendredi </label><br/>
-                                <input type="checkbox" name="Samedi" id="Samedi" class="td3"/> <label for="Samedi"> Samedi </label><br/>
-                                <input type="checkbox" name="dimanche" id="dimanche" class="td3"/> <label for="dimanche"> Dimanche </label><br/>
-                            </p>
-                            </form>                            
+                                            <p> <input type="radio"  name="recurrence" value="alldays" id="alldays" class="td3"/> <label for="alldays"> Tous les jours </label><br>
+                                                <input type="radio" name="recurrence" id="lundivendredi" value="lundivendredi" class="td3"/> <label for="lundivendredi"> Lundi au Vendredi </label><br/>
+                                                <input type="radio" name="recurrence" id="weekend" value="weekend" class="td3"/> <label for="weekend"> Samedi et Dimanche </label><br/>
+                                                
+                                            </p>
+                                    </div>  
+                            </div> 
+
+                            <div class="box5">
+                                   <input class="enre" type="submit" id="enre" name="enre" value="Enregistrer"/>
+                          
+                            </div>  
+
+                                            
+                    </form>
+                           
                 </div>
-                <div class="box5">
-                    <a href="#"><div class="enre"> Enregistrer </div></a>
-                </div>
-                </div>
+            
 
                 
                 <div class="box4">
